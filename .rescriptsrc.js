91368a567cb8109ab9b159bc294afcb019a4ee82
Ryan Lewis <r2c2ryan@gmail.com>
2021-03-26 20:06:57 -0700

    update comments in rescripts

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -1,4 +1,5 @@
-//prevent tilde character in output - (files with this character is invalid for SP)
+//prevent tilde character in output 
+// (files with this character are invalid for SP2013)
 module.exports = (config) => {
   config.optimization.splitChunks.automaticNameDelimiter = "_";
   if (config.optimization.runtimeChunk) {
